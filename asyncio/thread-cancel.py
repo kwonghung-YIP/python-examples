c9a48fec78c70cd130778b97d19ff93cae5cea30
Hung Yip <kwonghung.yip@gmail.com>
2025-08-02 11:27:41 +0100

    update example

--- asyncio/thread-cancel.py
+++ asyncio/thread-cancel.py
@@ -2,60 +2,53 @@ import time
 import logging
 import asyncio
 
-LOG_FORMAT = '%(asctime)s [%(levelname)s] %(message)s'
-logging.basicConfig(format=LOG_FORMAT,level=logging.DEBUG)
-
-log = logging.getLogger(__name__)
-
 start_timestamp: float
 
-def tasklog(msg:str, *args):
-    task = asyncio.current_task()
-    elapsed = time.perf_counter() - start_timestamp
-    log.log(logging.INFO,"task[%s] after %d sec:",task.get_name(),elapsed)
+def add_elapse_time(record:logging.LogRecord) -> bool:
+    record.elapse = time.perf_counter() - start_timestamp
+    return True
+
+LOG_FORMAT = '%(asctime)s [%(levelname)s] : %(message)s'
+TASK_LOG_FORMAT = '%(asctime)s [%(levelname)s]|%(threadName)s|%(taskName)s|%(elapse)6.3f : %(message)s'
+logging.basicConfig(format=LOG_FORMAT,level=logging.DEBUG)
+
+formatter = logging.Formatter(fmt=TASK_LOG_FORMAT)
+
+handler = logging.StreamHandler()
+handler.setFormatter(formatter)
+handler.addFilter(add_elapse_time)
+
+log = logging.getLogger(__name__)
+log.propagate = False
+log.addHandler(handler)
 
 async def long_running():
-    tasklog("Hello")
-    task = asyncio.current_task()
-    tname = task.get_name()
+    log.info("long_running: started...")
 
-    s = time.perf_counter()
-    log.info("long_running_func[%s]: started...",tname)
-
-    cancelCnt = 0
-    while cancelCnt <= 3:
-        try:
-            elapsed = time.perf_counter() - s
-            log.info("long_running_func[%s]: elapsed: %d, time.sleep for 2 seconds...", tname, elapsed)
+    try:
+        while True:
+            log.info("long_running: time.sleep for 2 seconds...")
             time.sleep(2)
-            elapsed = time.perf_counter() - s
-            log.info("long_running_func[%s]: elapsed: %d, asyncio.sleep for 30 seconds...", tname, elapsed)
+            log.info("long_running: asyncio.sleep for 30 seconds...")
             await asyncio.sleep(30)
-            elapsed = time.perf_counter() - s
-            log.info("long_running_func[%s]: wake up after %d seconds", tname, elapsed)
-        except asyncio.CancelledError as err:
-            elapsed = time.perf_counter() - s
-            log.info("long_running_func[%s]: catch CancelledError %s after %d seconds", tname, err, elapsed)
-            raise err
-
-    elapsed = time.perf_counter() - s
-    log.info("long_running_func[%s]: completed",tname)
+            log.info("long_running: wake up after %d seconds", 30)
+    except asyncio.CancelledError as err:
+        log.info("long_running: catch CancelledError %s", err)
+        #raise err
+    finally:
+        log.info("long_running_func: completed")
 
 async def report_status(target:asyncio.Task):
-    s = time.perf_counter()
     while not target.done():
         await asyncio.sleep(1)
-        elapsed = time.perf_counter() - s
-        log.info("task[%s] report status after %d seconds: cancelled[%s], done[%s]", target.get_name(), elapsed, target.cancelled(), target.done())    
+        log.info("task[%s] report status: cancelled[%s], done[%s]", target.get_name(), target.cancelled(), target.done())    
 
 async def cancel_task(target:asyncio.Task):
-    s = time.perf_counter()
     log.info("start cancel task...")
     while not target.done():
         await asyncio.sleep(10)
-        elapsed = time.perf_counter() - s
         successCancel = target.cancel("cancel() request from canel_task")
-        log.info("cancel task[%s] after %d seconds success? %s, cancelled? %s cancelling? %d", target.get_name(), elapsed, successCancel, target.cancelled(), target.cancelling())
+        log.info("cancel task[%s] success? %s, cancelled? %s cancelling? %d", target.get_name(), successCancel, target.cancelled(), target.cancelling())
     
 async def main():
     t1 = asyncio.create_task(long_running(),name="t1")
@@ -70,5 +63,4 @@ if __name__ == "__main__":
 
     asyncio.run(main())
 
-    elapsed = time.perf_counter() - start_timestamp
-    log.info(f"{__file__} executed in {elapsed} seconds.")
+    log.info(f"{__file__} executed.")
