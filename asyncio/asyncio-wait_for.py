222f94682ad1c468e1ee1b1d2d41fa17a4428bbf
Hung Yip <kwonghung.yip@gmail.com>
2025-08-19 00:59:47 +0100

    Add example

---
+++ asyncio/asyncio-wait_for.py
@@ -0,0 +1,32 @@
+import asyncio
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+log = logging.getLogger("asyncLogger")
+
+async def running_for(seconds:float):
+    try:
+        log.info("start running_for")
+        await asyncio.sleep(seconds)
+        log.info("complete running_for")
+    except asyncio.CancelledError as err:
+        log.info("received CancelledError")
+        # populate the CancelledError in order to raise 
+        # the TimeoutError for asyncio.wait_for
+        raise err
+
+async def main():
+    try:
+        coroutine = running_for(30)
+        task = asyncio.create_task(coroutine,name="mytask")
+        await asyncio.wait_for(task,20)
+    except TimeoutError as err:
+        log.info("TimeoutError from asyncio.wait_for")
+
+    log.info(f"task done? {task.done()}")
+    for t in asyncio.all_tasks():
+        log.info(f"{t} is not yet finished.")
+
+if __name__ == "__main__":
+    asyncio.run(main())
