91b623840ff35965f336fbe4c13ee929ad26746b
Hung Yip <kwonghung.yip@gmail.com>
2025-08-26 01:19:53 +0100

    Add example

--- asyncio/barrier.py
+++ asyncio/barrier.py
@@ -1,7 +1,7 @@
 import asyncio
-import logging
-import time
 import random
+import time
+import logging
 
 start_timestamp: float
 
@@ -23,35 +23,41 @@ log = logging.getLogger(__name__)
 log.propagate = False
 log.addHandler(handler)
 
-async def countFor(semaphore:asyncio.Semaphore,cntFor:int) -> None:
+async def countFor(barrier:asyncio.Barrier,cntFor:int) -> None:
     try:
         delay = random.randint(1,10)
         log.info(f"init delay for {delay} sec...")
         await asyncio.sleep(delay)
-        log.info("waiting for others to release semaphore...")
-        async with semaphore:
-            log.info("acquire semaphore")
+        log.info("waiting for barrier.wait()...")
+        async with barrier:
+            log.info(f"barrier released")
             i = 1
             while i <= cntFor:
                 log.info(f"count:{i}")
-                await asyncio.sleep(1)      
+                await asyncio.sleep(1)
                 i += 1
-        log.info("completed countFor().")
+        log.info("completed countFor()")
     except asyncio.CancelledError as err:
         log.info("catch asyncio.CancelledError")
         raise err
     
+async def reporting(barrier:asyncio.Barrier) -> None:
+    while True:
+        log.info(barrier)
+        await asyncio.sleep(1)
+
 async def main() -> None:
-    semaphore = asyncio.Semaphore(2)
-    tasks = [ asyncio.create_task(countFor(semaphore,15),name=f"task-{i}") for i in range(1,11) ]
+    barrier = asyncio.Barrier(4)
+    tasks = [ asyncio.create_task(countFor(barrier,10),name=f"task-{i}") for i in range(1,11) ]
+    tasks.append(reporting(barrier))
     try:
-        async with asyncio.timeout(60) as cm:
+        async with asyncio.timeout(22) as cm:
             await asyncio.gather(*tasks)
     except TimeoutError:
         log.info("catch TimeoutError")
-    log.info("main() completed.")
+    log.info("completed main()")
 
 if __name__ == "__main__":
     start_timestamp = time.perf_counter()
     asyncio.run(main())
-    log.info("__main__ completed.")
+    log.info("completed __main__")
