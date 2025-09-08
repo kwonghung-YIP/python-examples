3d0b625cf2b35dc56e9e5eb41b912816c4ee7b1a
Hung Yip <kwonghung.yip@gmail.com>
2025-09-08 02:48:35 +0100

    update example

--- logging/main.py
+++ logging/main.py
@@ -1,17 +1,34 @@
 import asyncio
+import time
+import yaml
+from pathlib import Path
+
 import logging
 from logging.config import dictConfig
+
+class MyFilter(logging.Filter):
+
+    @classmethod
+    def factory(cls):
+        return MyFilter()
+    
+    def __init__(self):
+        self.__start_timestamp:float = time.perf_counter()
+
+    def filter(self, record:logging.LogRecord) -> bool:
+        record.elapse = time.perf_counter() - self.__start_timestamp
+        return True
 
 config = {
     "version": 1,
     "formatters": {
         "asyncio_task": {
-            "format": "%(asctime)s [%(levelname)s]|%(threadName)s|%(taskName)s|%(funcName)s : %(message)s"
+            "format": "%(asctime)s [%(levelname)s]|%(threadName)s|%(taskName)s|%(elapse)6.3f|%(funcName)s : %(message)s"
         }
     },
     "filters": {
-        "filter1": {
-            "name": "asyncio_log"
+        "myFilter": {
+            "()": "main.MyFilter.factory"
         }
     },
     "handlers": {
@@ -19,7 +36,7 @@ config = {
             "class": "logging.StreamHandler",
             "level": "INFO",
             "formatter": "asyncio_task",
-            "filters": ["filter1"]
+            "filters": ["myFilter"]
         }
     },
     "loggers": {
@@ -30,6 +47,8 @@ config = {
         }
     }
 }
+
+cfg = yaml.load_all(Path("logging-config.yaml").read_text())
 
 dictConfig(config)
 log = logging.getLogger("asyncio_log")
